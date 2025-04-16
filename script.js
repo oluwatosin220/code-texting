7f50a3a3be3080b5a32321d18d27af667f5349ba
0luwatosin220 <yetundeadepitan6@gmail.com>
2025-04-16 15:55:19 +0100

    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document. getElementById('navLinks');
+
+hamburger.onclick = () => {
+    navLinks.classList.toggle('active');
+};
